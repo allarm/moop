d4371675e85e7b8906fd8cfead095e4965687207
Andrey Che <andrey.chertolyas@gmail.com>
2019-07-22 16:30:30 +0800

    Notes are indexed 0..11 now (instead 1..12) to comply with integer notation

--- moop/base/note.py
+++ moop/base/note.py
@@ -2,40 +2,34 @@ import re
 
 
 class Constants(object):
-    # Physical constants
-    HEARING_RANGE = (20, 20000)  # 20 Hz to 20 kHz
-
-    # Notes
-    # https://en.wikipedia.org/wiki/Musical_note
-    # Main constant
-    # Piano range 21-108 (88 keys from A0 to C8)
-
-    # Everything is tied to MIDI note number. It's fundamental.
-
+    # MIDI note range is the main constant. Everything is based on it.
+    # I.e. piano keys range is 21-108 (88 keys from A0 to C8)
     MIDI_NOTE_RANGE = range(0, 128)
 
+    # Internal note notation
     NOTES = "C CD D DE E F FG G GA A AB B".split()
     BLACK_KEYS = "CD DE FG GA AB".split()
 
-    NOTES_DIC = dict(zip(range(1, 13), NOTES))
-    # {1: 'C',  2: 'CD', 3: 'D',   4: 'DE',
-    #  5: 'E',  6: 'F',  7: 'FG',  8: 'G',
-    #  9: 'GA', 10: 'A', 11: 'AB', 12: 'B'}
+    # Notes are in integer notation (0..11)
+    # https://en.wikipedia.org/wiki/Pitch_class#Integer_notation
+    NOTES_DIC = dict(zip(range(0, 12), NOTES))
+    # { 0:  'C', 1:  'CD', 2:  'D', 3:  'DE',
+    # 4:  'E', 5:  'F', 6:  'FG', 7:  'G',
+    # 8:  'GA', 9:  'A', 10: 'AB', 11: 'B' }
 
-    NOTES_NUM_DIC = dict(zip(NOTES, range(1, 13)))
-    # {'C': 1,  'CD': 2, 'D': 3,   'DE': 4,
-    #  'E': 5,  'F': 6,  'FG': 7,  'G': 8,
-    #  'GA': 9, 'A': 10, 'AB': 11, 'B': 12}
+    NOTES_NUM_DIC = dict(zip(NOTES, range(0, 12)))
+    # { 'C':  0, 'CD': 1, 'D':  2, 'DE': 3,
+    # 'E':  4, 'F':  5, 'FG': 6, 'G':  7,
+    # 'GA': 8, 'A':  9, 'AB': 10, 'B':  11 }
 
     # Octaves
-
     # https://en.wikipedia.org/wiki/Octave
     # 11 octaves
-    # Using scientific notation (-1...11)
     # MIDI notation is (-5...5)
 
     OCTAVES_RANGE = range(-1, 12)
 
+    # Scientific notation (-1...11)
     OCTAVES_DIC = {
         -1: 'Dbl Contra',
         0:  'Sub Contra',
@@ -54,7 +48,9 @@ class Constants(object):
 
 
 class Note(object):
-    """Class Note """
+    """
+    Note
+    """
 
     # def __init__(self, *args, **kwargs):
     def __init__(self, value=None):
@@ -76,14 +72,13 @@ class Note(object):
     @property
     def is_black_key(self):
         """Returns true if note is a black key"""
-        return (Constants.NOTES_DIC[self._midi_note_number % 12 + 1]
+        return (Constants.NOTES_DIC[self._midi_note_number % 12]
                 in Constants.BLACK_KEYS)
 
     @property
     def is_white_key(self):
         """Returns true if note is a white key"""
-        # return not self.is_black_key(self)
-        return not (Constants.NOTES_DIC[self._midi_note_number % 12 + 1]
+        return not (Constants.NOTES_DIC[self._midi_note_number % 12]
                     in Constants.BLACK_KEYS)
 
     @property
@@ -99,7 +94,7 @@ class Note(object):
     @property
     def note_name(self):
         """Returns the name of the note"""
-        return Constants.NOTES_DIC[self._midi_note_number % 12 + 1]
+        return Constants.NOTES_DIC[self._midi_note_number % 12]
 
     @property
     def note_base_name(self):
@@ -119,12 +114,12 @@ class Note(object):
         """
         if self.is_black_key:
             return "{note_1}#{octave}/{note_2}♭{octave}".format(
-                    note_1=Constants.NOTES_DIC[self._midi_note_number % 12 + 1][0],
-                    note_2=Constants.NOTES_DIC[self._midi_note_number % 12 + 1][1],
+                    note_1=Constants.NOTES_DIC[self._midi_note_number % 12][0],
+                    note_2=Constants.NOTES_DIC[self._midi_note_number % 12][1],
                     octave=self.note_sci_octave)
 
         return "{}{}".format(
-                Constants.NOTES_DIC[self._midi_note_number % 12 + 1],
+                Constants.NOTES_DIC[self._midi_note_number % 12],
                 self.note_sci_octave)
 
     @note.setter
@@ -206,7 +201,7 @@ class Note(object):
 
         # return the MIDI number
         return (12 * (_input_octave + 1) +
-                Constants.NOTES_NUM_DIC[_input_note] - 1)
+                Constants.NOTES_NUM_DIC[_input_note])
 
     def __add__(self, other):
         """
